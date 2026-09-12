7804f97d6e8e3a7df2926e6bb4eb2fe9bbc80681
Ofloo <2203497+Ofloo@users.noreply.github.com>
2026-09-12 14:45:03 +0200

    Squashed 'webui/' content from commit b782a57631
    git-subtree-dir: webui
    git-subtree-split: b782a5763136653629fbf31bab59d30c1109be99

---
+++ src/app/helptext/about.ts
@@ -0,0 +1,33 @@
+import { T } from '../translate-marker';
+
+export default {
+  tagline: T('Welcome to your new NAS'),
+  help: T('Looking for help?'),
+
+  contactA: T('Does your business need'),
+  contactB: T('Enterprise level'),
+  contactC: T('support and services? Contact'),
+  contactD: T('iXsystems for more information.'),
+
+  opensourceA: T('is Free and'),
+  opensourceB: T('Open Source'),
+  opensourceC: T('software, which is provided as-is with no warranty.'),
+
+  findagainA: T('Find this information later by clicking'),
+  findagainB: T('(top-right), then'),
+  findagainC: T('About.'),
+
+  docs: T('The <a href="https://www.truenas.com/docs/" target="_blank">TrueNAS Documentation Site</a> \
+is a collaborative website with helpful guides and information about your new storage system.'),
+
+  forums: T('The <a href="https://forums.truenas.com/" target="_blank" class="external-link">TrueNAS Community Forums</a> \
+are the best place to ask questions and interact with fellow TrueNAS users.'),
+
+  newsLetter: T('You can join the <a href="https://www.truenas.com/newsletter/" target="_blank" class="external-link">TrueNAS Newsletter</a> \
+for monthly updates and latest developments.'),
+
+  actionBtnText: {
+    splash: T('Get Started'),
+    about: T('Close'),
+  },
+};
